be544abe0e2d2f84e6b400a8e357870daa09a7fb
kathleenfrench <kfrench09@gmail.com>
2020-07-31 13:16:12 -0400

    add completion cmd

--- cmd/pls/init.go
+++ cmd/pls/init.go
@@ -31,6 +31,7 @@ func addTopLevelSubcommands() {
 	rootCmd.AddCommand(checkCmd)
 	rootCmd.AddCommand(makeCmd)
 	rootCmd.AddCommand(updateCmd)
+	rootCmd.AddCommand(completionCmd)
 }
 
 func setPlsStyling() {
